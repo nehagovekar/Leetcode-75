fd4dd6420aa4c07c7d0e8523238eeab73aae30e1
nehagovekar <nehagovekar2198@gmail.com>
2025-07-14 14:38:02 -0700

    added comments and syntax imo

--- valid_anagram.py
+++ valid_anagram.py
@@ -25,15 +25,33 @@ class Solution:
             return False
         #creating dictionaries for both
         countS, countT = {}, {}
-
+        #gets tricky, since we want to create dictionaries where keys are letters and values are count lets iterate
         for i in range(len(s)):
-
+            #here-->s[i] is the letter of string s --> counts[LETTER]
             countS[s[i]] = 1 + countS.get(s[i],0)          
             countT[t[i]]= 1 + countT.get(t[i],0)
 
         return countS==countT
 
 
-       
+       '''
+       Input: s = "anagram", t = "nagaram"
+
+i	s[i]	t[i]	countS	                  countT
+0	a	    n	{'a': 1}	                       {'n': 1}
+1	n	    a	{'a': 1, 'n': 1}	               {'n': 1, 'a': 1}
+2	a	    g	{'a': 2, 'n': 1}	               {'n': 1, 'a': 1, 'g': 1}
+3	g	    a	{'a': 2, 'n': 1, 'g': 1}	       {'n': 1, 'a': 2, 'g': 1}
+4	r   	r	{'a': 2, 'n': 1, 'g': 1, 'r': 1}    	same
+5	a   	a	{'a': 3, ...}	{'a': 3, ...}
+6	m   	m	{'a': 3, ..., 'm': 1}	same'''
+
+'''
+s[i]                # Get the character at position i in string s
+countS[s[i]]        # Access the count value of that character in the dictionary
+countS.get(s[i], 0) # Try to get the count for s[i]; return 0 if s[i] is not found
+1 + countS.get(...) # Add 1 to the current count (or to 0 if the character is new)
+countS[s[i]] = ...  # Store the updated count for that character back in the dictionary
+'''
 
         
