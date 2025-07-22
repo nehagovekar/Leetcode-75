f42a1c28ce89fcdcfdb45a0d78820dcaee268759
nehagovekar <nehagovekar2198@gmail.com>
2025-07-21 20:16:19 -0700

    pracmon

--- Top K Frequent Elements.py
+++ Top K Frequent Elements.py
@@ -24,24 +24,29 @@ Constraints:
 1 <= k <= number of distinct elements in nums.
 '''
 
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        #create a dictionary for counting frequency
-        count = {}
-        #create an array for the count-value(list of nums) which is freq
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        #1.create a hashmap for count
+        count= {}
+        #create a list of lists to have index (imaginary) and values (list of numbers that have the same count)
+      #  freq = [[] for i in range(0, len(nums))]
         freq= [[] for i in range(len(nums)+1)]
-        #simple count how many times each number has occured
 
-        for i in nums:
-            count[i] =1 + count.get(i,0)
 
-        for num,cnt in count.items():
-            freq[cnt].append(num)
+        #count the frequency of numbers by iterating the nums now
+        for n in nums:
+            count[n]=1 + count.get(n,0)
+        
+        #here, we will have the dicionary with key values which we will traverse to add that to the list freq
+        for n,c in count.items():
+            freq[c].append(n)
+     
+
+        #now we need a result list which has the top k elements
         #Okay, i got confused with res
         #1. remember traversing through range is through indices
         #so once, inside (you want to go through the list in each index hence the second loop)
         #now after appending when it equals to res list then return
-
         res=[]
         for i in range(len(freq)-1 ,0, -1):
             for num in freq[i]:
@@ -49,8 +54,17 @@ class Solution:
                 if len(res)==k:
                     return res
 
-            
-       
+
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+'''
+        mistake 1: freq[c].append(n) -->dont try to append -->freq.append you are appending in that index thats the point
+        mistake 2: after one loop to iterate ulta dont forget each i has list so inner foor loop is necessary
+        mistake 3: when creating a list of list dont think of it like a dict
+        '''
 
 
         
